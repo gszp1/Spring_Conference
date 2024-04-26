18dacf2e36412a0d1a64feed20fcfc6ad7e10377
gszp1 <grzybekmateusz@poczta.fm>
2024-04-26 23:49:44 +0200

    Adds Presentation entity.

---
+++ src/main/java/org/example/spring_conference/model/Presentation.java
@@ -0,0 +1,20 @@
+package org.example.spring_conference.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "presentations")
+public class Presentation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "presentation_id")
+    private int presentationId;
+}
