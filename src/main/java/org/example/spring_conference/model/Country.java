6d2fcdbdce5c8b1901c3e1d1aa733434db82b3a8
gszp1 <grzybekmateusz@poczta.fm>
2024-04-26 23:43:11 +0200

    Adds Country entity.

---
+++ src/main/java/org/example/spring_conference/model/Country.java
@@ -0,0 +1,23 @@
+package org.example.spring_conference.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_id")
+    private int countryId;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+}
