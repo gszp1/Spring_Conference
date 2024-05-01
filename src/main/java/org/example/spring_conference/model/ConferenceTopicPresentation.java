74d190a635dd80d519dcfa9c4428cb87959ff159
gszp1 <grzybekmateusz@poczta.fm>
2024-05-01 20:04:34 +0200

    Adds ConferneceTopicPresentation join table entity.

--- src/main/java/org/example/spring_conference/model/ConferenceTopicPresentation.java
+++ src/main/java/org/example/spring_conference/model/ConferenceTopicPresentation.java
@@ -1,4 +1,26 @@
 package org.example.spring_conference.model;
 
+import jakarta.persistence.*;
+import org.example.spring_conference.compositekeys.ConferenceTopicKey;
+
+@Entity
+@Table(name = "conference_topic_presentations")
 public class ConferenceTopicPresentation {
+
+    @EmbeddedId
+    private ConferenceTopicKey id;
+
+    @ManyToOne
+    @MapsId("conferenceId")
+    @JoinColumn(name = "FK_conference", nullable = false)
+    private Conference conference;
+
+    @ManyToOne
+    @MapsId("topicId")
+    @JoinColumn(name = "FK_topic", nullable = false)
+    private Topic topic;
+
+    @OneToOne
+    @JoinColumn(name = "FK_presentation", nullable = false, unique = true)
+    private Presentation presentation;
 }
