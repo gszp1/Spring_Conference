d5337d2fe69870a731bd24b015110b30f45b5bb0
gszp1 <grzybekmateusz@poczta.fm>
2024-05-01 19:24:58 +0200

    Adds ConferenceTopicPresentationKey embeddable class.

---
+++ src/main/java/org/example/spring_conference/compositekeys/ConferenceTopicPresentationKey.java
@@ -0,0 +1,20 @@
+package org.example.spring_conference.compositekeys;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Embeddable
+public class ConferenceTopicPresentationKey {
+
+    private int conferenceId;
+
+    private int topicId;
+
+    private int presentationId;
+}
