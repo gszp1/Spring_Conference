e02766f1227a4c006198c61edfc9a0920a5e70a9
gszp1 <grzybekmateusz@poczta.fm>
2024-05-03 20:53:17 +0200

    Adds save and saveAll methods to HotelService.

--- src/main/java/org/example/spring_conference/service/HotelService.java
+++ src/main/java/org/example/spring_conference/service/HotelService.java
@@ -1,8 +1,11 @@
 package org.example.spring_conference.service;
 
+import org.example.spring_conference.model.Hotel;
 import org.example.spring_conference.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HotelService {
@@ -12,5 +15,13 @@ public class HotelService {
     @Autowired
     public HotelService(HotelRepository hotelRepository) {
         this.hotelRepository = hotelRepository;
+    }
+
+    public Hotel save(Hotel hotel) {
+        return hotelRepository.save(hotel);
+    }
+
+    public List<Hotel> saveAll(List<Hotel> hotels) {
+        return hotelRepository.saveAll(hotels);
     }
 }
