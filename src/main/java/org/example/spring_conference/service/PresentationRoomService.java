625c93f844eef6ea616e03d29685dcecc21dd170
gszp1 <grzybekmateusz@poczta.fm>
2024-05-03 20:55:48 +0200

    Adds save and saveAll methods to PresentationRoomService.

--- src/main/java/org/example/spring_conference/service/PresentationRoomService.java
+++ src/main/java/org/example/spring_conference/service/PresentationRoomService.java
@@ -1,8 +1,11 @@
 package org.example.spring_conference.service;
 
+import org.example.spring_conference.model.PresentationRoom;
 import org.example.spring_conference.repository.PresentationRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PresentationRoomService {
@@ -12,5 +15,13 @@ public class PresentationRoomService {
     @Autowired
     public PresentationRoomService(PresentationRoomRepository presentationRoomRepository) {
         this.presentationRoomRepository = presentationRoomRepository;
+    }
+
+    public PresentationRoom save(PresentationRoom presentationRoom) {
+        return presentationRoomRepository.save(presentationRoom);
+    }
+
+    public List<PresentationRoom> saveAll(List<PresentationRoom> presentationRooms) {
+        return presentationRoomRepository.saveAll(presentationRooms);
     }
 }
