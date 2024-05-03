865463878a4ce13a6d539b76f4ac7df4e4814f60
gszp1 <grzybekmateusz@poczta.fm>
2024-05-03 20:54:16 +0200

    Adds save and saveAll methods to ParticipantService.

--- src/main/java/org/example/spring_conference/service/ParticipantService.java
+++ src/main/java/org/example/spring_conference/service/ParticipantService.java
@@ -1,8 +1,11 @@
 package org.example.spring_conference.service;
 
+import org.example.spring_conference.model.Participant;
 import org.example.spring_conference.repository.ParticipantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ParticipantService {
@@ -12,5 +15,13 @@ public class ParticipantService {
     @Autowired
     public ParticipantService(ParticipantRepository participantRepository) {
         this.participantRepository = participantRepository;
+    }
+
+    public Participant save(Participant participant) {
+        return participantRepository.save(participant);
+    }
+
+    public List<Participant> saveAll(List<Participant> participants) {
+        return participantRepository.saveAll(participants);
     }
 }
