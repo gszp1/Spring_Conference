17ed0f926b1ed604ee4f33aa473f2c19545195f1
gszp1 <grzybekmateusz@poczta.fm>
2024-05-03 20:51:39 +0200

    Adds save and saveAll to ConferenceTopicPresentationService.

--- src/main/java/org/example/spring_conference/service/ConferenceTopicPresentationService.java
+++ src/main/java/org/example/spring_conference/service/ConferenceTopicPresentationService.java
@@ -1,8 +1,11 @@
 package org.example.spring_conference.service;
 
+import org.example.spring_conference.model.ConferenceTopicPresentation;
 import org.example.spring_conference.repository.ConferenceTopicPresentationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ConferenceTopicPresentationService {
@@ -12,5 +15,13 @@ public class ConferenceTopicPresentationService {
     @Autowired
     public ConferenceTopicPresentationService(ConferenceTopicPresentationRepository conferenceTopicPresentationRepository) {
         this.conferenceTopicPresentationRepository = conferenceTopicPresentationRepository;
+    }
+
+    public ConferenceTopicPresentation save(ConferenceTopicPresentation conferenceTopicPresentation) {
+        return conferenceTopicPresentationRepository.save(conferenceTopicPresentation);
+    }
+
+    public List<ConferenceTopicPresentation> saveAll(List<ConferenceTopicPresentation> conferenceTopicPresentations) {
+        return conferenceTopicPresentationRepository.saveAll(conferenceTopicPresentations);
     }
 }
