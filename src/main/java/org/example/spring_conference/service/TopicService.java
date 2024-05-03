3f88515e0556dc0335c27f0e366899edc38f14e4
gszp1 <grzybekmateusz@poczta.fm>
2024-05-03 20:58:07 +0200

    Adds save and saveAll methods to TopicService.

--- src/main/java/org/example/spring_conference/service/TopicService.java
+++ src/main/java/org/example/spring_conference/service/TopicService.java
@@ -1,7 +1,10 @@
 package org.example.spring_conference.service;
 
+import org.example.spring_conference.model.Topic;
 import org.example.spring_conference.repository.TopicRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TopicService {
@@ -10,5 +13,13 @@ public class TopicService {
 
     public TopicService(TopicRepository topicRepository) {
         this.topicRepository = topicRepository;
+    }
+
+    public Topic save(Topic topic) {
+        return topicRepository.save(topic);
+    }
+
+    public List<Topic> saveAll(List<Topic> topics) {
+        return topicRepository.saveAll(topics);
     }
 }
