55dd3439f1abb70592a2cb6e8c5736c5c6f58cb8
gszp1 <grzybekmateusz@poczta.fm>
2024-05-03 20:57:25 +0200

    Adds save and saveAll methods to RoleService.

--- src/main/java/org/example/spring_conference/service/RoleService.java
+++ src/main/java/org/example/spring_conference/service/RoleService.java
@@ -1,8 +1,11 @@
 package org.example.spring_conference.service;
 
+import org.example.spring_conference.model.Role;
 import org.example.spring_conference.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoleService {
@@ -12,5 +15,13 @@ public class RoleService {
     @Autowired
     public RoleService(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
+    }
+
+    public Role save(Role role) {
+        return roleRepository.save(role);
+    }
+
+    public List<Role> saveAll(List<Role> roles) {
+        return roleRepository.saveAll(roles);
     }
 }
